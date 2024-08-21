23fdc43b278da002d9bbfe9489d7a134b42d7388
ernesto <ernestoponce27@gmail.com>
2024-08-21 01:41:04 -0300

    add lambda go base

---
+++ lambda-apigateway/deploy.sh
@@ -0,0 +1,14 @@
+version=$1
+if [ -z "$version" ]; then
+    echo "Please provide a version argument."
+    exit 1
+fi
+
+
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 383660184915.dkr.ecr.us-west-2.amazonaws.com
+
+docker build -t lambda-gateway:$version .
+
+docker tag lambda-gateway:$version 383660184915.dkr.ecr.us-west-2.amazonaws.com/lambda-gateway:$version
+
+docker push 383660184915.dkr.ecr.us-west-2.amazonaws.com/lambda-gateway:$version
---
+++ lambda-apigateway/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(ctx context.Context, event events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("event", event)
+
+	for k, v := range event.Headers {
+		fmt.Printf("header %s: %s\n", k, v)
+	}
+
+	for k, v := range event.QueryStringParameters {
+		fmt.Printf("query %s: %s\n", k, v)
+	}
+
+	for k, v := range event.PathParameters {
+		fmt.Printf("path %s: %s\n", k, v)
+	}
+
+	for k, v := range event.StageVariables {
+		fmt.Printf("stage %s: %s\n", k, v)
+	}
+
+	fmt.Println("requestContext", event.RequestContext)
+
+	response := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello " + event.RawPath,
+	}
+	return response, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
